116ce1848099a35b5480f5fa3bc575a6187f88c2
Sairindhri Bhattacharya <sairin@umich.edu>
2023-06-20 08:26:18 -0700

    web app files under src

--- src/app.py
+++ src/app.py
@@ -8,42 +8,44 @@ from urllib.parse import quote
 
 app = Flask(__name__)
 
-restaurant_philadelphia_df = pickle.load(open('/Users/sairindhri/yelp-data-analysis2/src/data/top_restaurant_philadelphia.pkl','rb'))
-prediction_model = pickle.load(open('/Users/sairindhri/yelp-data-analysis2/src/data/svm_sentiment_prediction_model.pkl','rb'))
-
-
+restaurant_sentiment_final = pickle.load(open('/Users/sairindhri/yelp-data-analysis2/src/data/restaurant_sentiment_final.pkl','rb'))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',business_id = list(restaurant_sentiment_final['business_id'].values), name = list(restaurant_sentiment_final['name'].values))
 
 @app.route('/restaurant_details', methods=['POST'])
 @cross_origin(origins=['http://localhost:5000', 'http://127.0.0.1:5000'])
 def display_details():
-    user_input = request.form.get('myselect')
+    business_id = request.form.get('myselect')
 
     API_KEY = 'AIzaSyB2NzwqnLD20WafDa4nEBe4-SnZHjxETbU'
-    address = '1600 Amphitheatre Parkway, Mountain View, CA' # replace with your address
-
-    # Step 1: Get Place data from address
-    response = requests.get(f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={address}&inputtype=textquery&fields=photos&key={API_KEY}")
-    data = response.json()
-
-    photoUrl = ""
-
-    if 'candidates' in data and len(data['candidates']) > 0 and 'photos' in data['candidates'][0] and len(data['candidates'][0]['photos']) > 0:
-        photoReference = data['candidates'][0]['photos'][0]['photo_reference']
-        print("photoReference:", photoReference)
-        # Step 2: Get Photo from Photo Reference
-        photoUrl = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photoReference}&key={API_KEY}"
-        # photoUrl.replace('&amp;', '&')
-        print("photoUrl:", photoUrl)
 
     # selected_restaurant = request.form.get('myselect')
     # all_restaurants = request.form.get('allRestaurants').split(',')
     # continue with your logic here...
-    print(str(user_input))
-    return render_template('restaurant_details.html', photo_url=photoUrl)
+    print("business_id = {}".format(business_id))
+    index = np.where(restaurant_sentiment_final.business_id==business_id)[0][0]
+    target_row = restaurant_sentiment_final.iloc[index]
+    print("index = {}".format(index))
+    name=target_row['name']
+    average_stars=np.round(target_row['average_star'],2)
+    negative_reviews=target_row['negative_reviews']
+    positive_reviews=target_row['positive_reviews']
+    neutral_reviews=target_row['neutral_reviews']
+    address=target_row['address']
+    city=target_row['city']
+    state=target_row['state']
+    full_address=quote(address + ', ' + city + ', ' + state)
+
+
+    return render_template('restaurant_details.html', 
+                           name=name, 
+                           average_stars=average_stars,
+                           negative_reviews=negative_reviews,
+                           neutral_reviews=neutral_reviews,
+                           positive_reviews=positive_reviews,
+                           full_address=full_address)
 
 if __name__ == '__main__':
     app.run(debug=True)
