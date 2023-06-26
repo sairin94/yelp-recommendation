c162266798961e7ffa25fbc542cf38cdf38126f1
Sairindhri Bhattacharya <sairin@umich.edu>
2023-06-26 00:30:13 -0700

    Working hosted website

--- src/app.py
+++ src/app.py
@@ -8,44 +8,48 @@ from urllib.parse import quote
 
 app = Flask(__name__)
 
-restaurant_sentiment_final = pickle.load(open('/Users/sairindhri/yelp-data-analysis2/src/data/restaurant_sentiment_final.pkl','rb'))
+restaurant_aspect=pickle.load(open('./data/business_final_metric.pkl','rb'))
 
 @app.route('/')
 def index():
-    return render_template('index.html',business_id = list(restaurant_sentiment_final['business_id'].values), name = list(restaurant_sentiment_final['name'].values))
+    return render_template('index.html',business_id = list(restaurant_aspect['business_id'].values), name = list(restaurant_aspect['name'].values))
+
 
 @app.route('/restaurant_details', methods=['POST'])
 @cross_origin(origins=['http://localhost:5000', 'http://127.0.0.1:5000'])
-def display_details():
+def display_details_aspect():
     business_id = request.form.get('myselect')
 
     API_KEY = 'AIzaSyB2NzwqnLD20WafDa4nEBe4-SnZHjxETbU'
-
-    # selected_restaurant = request.form.get('myselect')
-    # all_restaurants = request.form.get('allRestaurants').split(',')
-    # continue with your logic here...
     print("business_id = {}".format(business_id))
-    index = np.where(restaurant_sentiment_final.business_id==business_id)[0][0]
-    target_row = restaurant_sentiment_final.iloc[index]
+    index = np.where(restaurant_aspect.business_id==business_id)[0][0]
+    target_row = restaurant_aspect.iloc[index]
     print("index = {}".format(index))
     name=target_row['name']
-    average_stars=np.round(target_row['average_star'],2)
-    negative_reviews=target_row['negative_reviews']
-    positive_reviews=target_row['positive_reviews']
-    neutral_reviews=target_row['neutral_reviews']
+    stars=np.round(target_row['stars'],2)
     address=target_row['address']
     city=target_row['city']
     state=target_row['state']
     full_address=quote(address + ', ' + city + ', ' + state)
+    sentiment_score=target_row['sentiment_score']
+    food_quality=target_row['food_quality']
+    price=target_row['price']
+    ambiance=target_row['ambiance']
+    service=target_row['service']
+    comfort=target_row['comfort']
 
 
     return render_template('restaurant_details.html', 
                            name=name, 
-                           average_stars=average_stars,
-                           negative_reviews=negative_reviews,
-                           neutral_reviews=neutral_reviews,
-                           positive_reviews=positive_reviews,
-                           full_address=full_address)
+                           stars=stars,
+                           full_address=full_address,
+                           sentiment_score=sentiment_score, 
+                           food_quality=food_quality,
+                           price=price,
+                           ambiance=ambiance,
+                           service=service,
+                           comfort=comfort)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
